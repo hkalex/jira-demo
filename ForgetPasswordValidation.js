3493815d5c13ba0713d1d220e4a2a0f4ed26ac45
Alex Yeung <hkalex@gmail.com>
2017-06-15 22:09:32 +1000

    HD-13 Email validation done

---
+++ ForgetPasswordValidation.js
@@ -0,0 +1,6 @@
+module.exports = function({email}) {
+  if (!email) return 'Email cannot be empty';
+  var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var result = reg.test(email);
+  return !!result;
+}
